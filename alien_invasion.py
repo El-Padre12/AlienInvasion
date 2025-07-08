d1f6e1da6ee14da775066a42d6f6d38546150e17
AngelC <noel0402.ac@gmail.com>
2025-07-08 18:50:56 -0500

    changed screen size

--- alien_invasion.py
+++ alien_invasion.py
@@ -18,7 +18,7 @@ class AlienInvasion:
         # elements in the game such as 'alien' or 'ship' are surfaces
         self.clock = pygame.time.Clock()
         self.settings = Settings()        
-        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.screen = pygame.display.set_mode((1600,900)) # prefer this size over fullscreen
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Free My Boy E.T. 2025")
