683e833b1d6f9b90ba85f85f49fd8caceee54676
AngelC <noel0402.ac@gmail.com>
2025-07-08 16:41:16 -0500

    added code that limits number of bullets the ship can fire at one time

--- alien_invasion.py
+++ alien_invasion.py
@@ -80,9 +80,9 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """creates and adds new bullet to bullets group"""
-
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
     def _update_screen(self):
         """updates images on the screen and flip to the new screen"""
