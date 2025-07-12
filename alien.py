236338fbbe87107513eeda956d3a8070d0e1b239
AngelC <noel0402.ac@gmail.com>
2025-07-12 17:50:28 -0500

    added alien class

---
+++ alien.py
@@ -0,0 +1,22 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Alien(Sprite):
+    """Alien class to manage alien fleet"""
+
+    def __init__(self, ai_game):
+        """inits the alien and sets starting position"""
+
+        super().__init__()
+        self.screen = ai_game.screen
+
+        # loads image and sets it rect attribute
+        self.image = pygame.image.load('images/')
+        self.rect = self.image.get_rect()
+
+        # starts each new alien on the top left of the screen
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        # stores the aliens exact horizontal positions
+        self.x = float(self.rect.x)
